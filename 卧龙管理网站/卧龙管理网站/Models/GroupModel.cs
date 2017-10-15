554393dde3c2332728c4b28f0c17bb563498885c
lingxiezfy <1241948182@qq.com>
2017-10-15 16:47:24 +0800

    添加GroupModel

---
+++ 卧龙管理网站/卧龙管理网站/Models/GroupModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WollonMe.Models
+{
+    public class GroupModel
+    {
+        [Key]
+        public int GroupID { get; set; }
+        public string GroupName { get; set; }
+
+    }
+}
