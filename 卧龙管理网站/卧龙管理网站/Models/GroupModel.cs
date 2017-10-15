71ad19e53fbbf46c836b9af169fba30e19e0f723
马玉琛 <JimMa0312@outlook.com>
2017-10-15 17:00:44 +0800

    重新上传GroupModel

---
+++ 卧龙管理网站/卧龙管理网站/Models/GroupModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WollonMe.Models
+
+{
+
+    public class GroupModel
+
+    {
+
+        [Key]
+
+        public int GroupID { get; set; }
+
+        public string GroupName { get; set; }
+    }
+
+}
