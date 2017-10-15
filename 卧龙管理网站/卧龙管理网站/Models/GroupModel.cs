b6703fa84dc152e08b361770b6038de0bb621445
马玉琛 <JimMa0312@outlook.com>
2017-10-15 16:56:46 +0800

    修正无GroupModel.cs

--- 卧龙管理网站/卧龙管理网站/Models/GroupModel.cs
+++ 卧龙管理网站/卧龙管理网站/Models/GroupModel.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace WollonMe.Models
+
 {
+
     public class GroupModel
+
     {
+
         [Key]
+
         public int GroupID { get; set; }
+
         public string GroupName { get; set; }
 
+
+
     }
+
 }
