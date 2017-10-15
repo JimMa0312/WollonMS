3aaa530dce13cc122ea6c46ca937d15dfa517be2
lingxiezfy <1241948182@qq.com>
2017-10-15 17:07:59 +0800

    添加PositionModel

---
+++ 卧龙管理网站/卧龙管理网站/Models/PositionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WollonMe.Models
+{
+    public class PositionModel
+    {
+        [Key]
+        public int PositionID { get; set; }
+        public string PositionName { get; set; }
+    }
+}
